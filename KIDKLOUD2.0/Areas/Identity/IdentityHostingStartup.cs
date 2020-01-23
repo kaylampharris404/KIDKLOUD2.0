8590da637469142925373a608aef03be92dd1931
kaylampharris <kaylampharris@outlook.com>
2020-01-23 15:38:41 -0600

    Add project files.

---
+++ KIDKLOUD2.0/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using KIDKLOUD2._0.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(KIDKLOUD2._0.Areas.Identity.IdentityHostingStartup))]
+namespace KIDKLOUD2._0.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ KIDKLOUD2.0/Controllers/ChildrenController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KidKloud.Controllers
+{
+    public class ChildrenController : Controller
+    {
+        // GET: Children
+        public ActionResult Index()
+        {
+            var model = new List<string>()
+            {
+                "Click to Choose Child...","Maleah M. Brown","Jordan A. Brown"
+            };
+            return View(model);
+        }
+    }
+}
